b318f3e2d925f771dddff9be2200a766fad67a30
GerriF <155729912+KleinerBaum@users.noreply.github.com>
2025-06-18 13:48:21 +0200

    Update config.js

--- magicmirror/config/config.js
+++ magicmirror/config/config.js
@@ -21,29 +21,20 @@ let config = {
       module: "MMM-AEMet",
       position: "top_left",
       config: {
-        apiKey: "<DEIN_AEMET_KEY>",
+        apiKey: "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJnZXJyaXQuZmFiaXNjaDIwMjRAZ21haWwuY29tIiwianRpIjoiZTlhMWE0ZDktMWJlMC00NTkwLTlkODgtNDYxMzY1ZThkN2IwIiwiaXNzIjoiQUVNRVQiLCJpYXQiOjE3NTAxOTI5MDgsInVzZXJJZCI6ImU5YTFhNGQ5LTFiZTAtNDU5MC05ZDg4LTQ2MTM2NWU4ZDdiMCIsInJvbGUiOiIifQ.9fIBYzJoFXKlLWe0UUOVmW0Usym1cvEVEXHPRbeUnQI",
         lat: 36.721,
         lon: -4.421
       }
     },
-    {
-      module: "MMM-PirateWeather",
-      position: "top_right",
-      config: {
-        apiKey: "<DEIN_PIRATE_KEY>",
-        lat: 36.721,
-        lon: -4.421
-      }
-    },
-    {
+       {
       module: "MMM-WindyForecast",
       position: "top_center",
       config: {
         lat: 36.721,
         lon: -4.421,
         overlay: "wind",
-        pointKey: "<DEIN_POINT_KEY>",
-        webcamKey: "<DEIN_WEBCAM_KEY>"
+        pointKey: "<jmbOoi5E6wcbBopBC2SSctlrFg4kRQ3l>",
+        webcamKey: "bqP18j779vqftLdj98RzHKMsbdABP2f7"
       }
     }
     // Füge max. 1-2 weitere leichte Module hinzu!
