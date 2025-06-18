14f0eced6a82a9b4de0dab709cec5b1dad20821b
GerriF <155729912+KleinerBaum@users.noreply.github.com>
2025-06-18 13:39:25 +0200

    chore: update config for surf modules

--- magicmirror/config/config.js
+++ magicmirror/config/config.js
@@ -16,6 +16,35 @@ let config = {
         showWind: false,    // weniger Grafik = weniger RAM
         locale: "es",
       }
+    },
+    {
+      module: "MMM-AEMet",
+      position: "top_left",
+      config: {
+        apiKey: "<DEIN_AEMET_KEY>",
+        lat: 36.721,
+        lon: -4.421
+      }
+    },
+    {
+      module: "MMM-PirateWeather",
+      position: "top_right",
+      config: {
+        apiKey: "<DEIN_PIRATE_KEY>",
+        lat: 36.721,
+        lon: -4.421
+      }
+    },
+    {
+      module: "MMM-WindyForecast",
+      position: "top_center",
+      config: {
+        lat: 36.721,
+        lon: -4.421,
+        overlay: "wind",
+        pointKey: "<DEIN_POINT_KEY>",
+        webcamKey: "<DEIN_WEBCAM_KEY>"
+      }
     }
     // Füge max. 1-2 weitere leichte Module hinzu!
   ]
